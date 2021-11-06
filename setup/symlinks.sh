004ad2cd6f6e1e361ef03037c5526f5e8efc6019
Charles Vincent <cjmvincent@outlook.com>
2021-11-06 17:58:29 -0400

    change from symlinks to just a copy of

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ################################################################################################################
-# This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/dotfiles          #
+# This script creates copies from the home directory of any desired dotfiles in ${homedir}/dotfiles          #
 ################################################################################################################
 
 # dotfiles directory
@@ -12,11 +12,11 @@ echo "Changing to the ${dotfiledir} directory"
 cd ${dotfiledir}
 echo "...done"
 
-# list of files/folders to symlink in ${homedir}
+# list of files/folders to copy in ${homedir}
 files=(atom config init mackup.cfg)
 
-# create symlinks (will overwrite old dotfiles)
+# create copies (will overwrite old dotfiles)
 for file in ${files}; do
     echo "Creating symlink to $file in home directory."
-    ln -sf ${dotfiledir}/.${file} $HOME/.${file}
+    cp -R ${dotfiledir}/${file} $HOME/.${file}
 done
