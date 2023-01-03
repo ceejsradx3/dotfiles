497e8315b5162bd73564031e769e7b8f22d91843
Charles Vincent <cjmvincent@outlook.com>
2023-01-03 13:51:34 -0500

    .

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Setting up your Mac..."
 
 echo "Installing Homebrew, your desired packages, and applications..."
 # run the Homebrew Script
-source ~/dotfiles/setup/brew.sh
+source ./setup/brew.sh
 
 echo "Restoring your mackup backup..."
 # restore backup
@@ -12,12 +12,13 @@ mackup restore
 
 echo "Loading your MacOS preferences.."
 # load macOS preferences
-source ~/dotfiles/macos/macos
+source ./macos/macos
 
 echo "Configuring your dock..."
 # replace dock apps
-source ~/dotfiles/macos/dock.sh
+source ./macos/dock.sh
 
 echo "Cleaning up my mess..."
 # remove dotfiles folder
-sudo rm -r -f ~/dotfiles/
+cd ..
+sudo rm -r -f /dotfiles/
