733a19ec5163d436297b8c13715faa6200637bb0
Charles Vincent <cjmvincent@outlook.com>
2023-04-10 18:40:26 -0400

    add powerlevel10k to installation

--- setup/install.sh
+++ setup/install.sh
@@ -75,3 +75,6 @@ mas install bettersnaptool
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
