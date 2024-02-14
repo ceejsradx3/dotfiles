f4d5122ee6e7cf73fe84556abbbb6afab42c3414
Charles Vincent <cjmvincent@outlook.com>
2024-02-14 14:50:35 -0500

    iteration

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+echo "Installing Homebrew, your desired packages, and applications..."
 
 # Check if homebrew is installed
 if test ! $(which brew); then
@@ -21,8 +25,7 @@ brew update
 brew tap homebrew/bundle
 brew bundle --file ./setup/brewfile
 
-brew services start skhd
-
+echo "Configuring your terminal..."
 # install oh-my-zsh
 if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
@@ -30,3 +33,23 @@ fi
 
 # install Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# restore backup
+echo "Restoring your mackup backup..."
+mackup restore
+
+# load macOS preferences
+echo "Loading your MacOS preferences.."
+source ./setup/macos
+
+# replace dock apps
+echo "Configuring your dock..."
+source ./setup/dock.sh
+
+# remove dotfiles folder
+echo "Cleaning up my mess..."
+cd ..
+sudo rm -r -f /dotfiles/
+
+# start install services and plugins
+brew services start skhd
