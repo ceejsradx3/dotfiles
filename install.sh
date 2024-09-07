692586ea9896ffd67807a346e9b5a1bc0dae9332
Charles Vincent <cjmvincent@outlook.com>
2024-09-07 16:06:15 -0400

    making it cute

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/sh bash
 
 clear
+echo
 echo " _           _        _ _       _     "
 echo "(_)         | |      | | |     | |    "
 echo " _ _ __  ___| |_ __ _| | |  ___| |__  "
@@ -14,32 +15,39 @@ echo Please enter root password
 # Ask for the administrator password upfront.
 sudo -v
 
+echo
 echo "Setting up your Mac..."
+echo
 
 # check for and install homebrew
-echo 
+echo
+echo "Installing Homebrew and then desired apps..."
+echo
 source ./setup/brew.sh
 
 # configure terminal by installing ohmyzsh and powerlevel10k theme
 echo 
 source ./setup/terminal.sh
+echo
 
 # restore backup
+echo
 echo "Restoring your mackup backup..."
+echo
 mackup restore
 
 # load macOS preferences
+echo
 echo "Loading your MacOS preferences.."
+echo
 source ./setup/macos
 
 # replace dock apps
+echo
 echo "Configuring your dock..."
+echo
 source ./setup/dock.sh
 
-# remove dotfiles folder
-#echo "Cleaning up my mess..."
-#cd ..
-#sudo rm -r -f /dotfiles/
 
 echo "______ _____ _   _  _____ "
 echo "|  _  \  _  | \ | ||  ___|"
