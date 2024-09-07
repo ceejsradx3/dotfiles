bf6f4fda278f096a9ba68c9e2114a8c530540c51
Charles Vincent <cjmvincent@outlook.com>
2024-09-07 15:00:54 -0400

    make homebrew install silent

--- setup/brew.sh
+++ setup/brew.sh
@@ -3,7 +3,7 @@ echo "Installing Homebrew, your desired packages, and applications..."
 
 # Check if homebrew is installed
 if test ! $(which brew); then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Uncomment this section if you are running a Mac with an Apple Silicon processor
 # Be sure to change username here!
