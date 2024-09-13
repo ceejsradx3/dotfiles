451844e89e094a882d6346058b9327c2d72da27f
Charles Vincent <cjmvincent@outlook.com>
2024-09-13 10:30:28 -0400

    terminal themes and plugins

--- setup/terminal.sh
+++ setup/terminal.sh
@@ -12,6 +12,8 @@ fi
 # install Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# install a couple of zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# remaining changes will take place when mackup restores my dotfiles
+# remaining changes will take place when dotfiles are installed
