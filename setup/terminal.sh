5a15235ba761cd32c6796a7d437b71f753c6c899
Charles Vincent <cjmvincent@outlook.com>
2024-10-23 10:05:19 -0400

    swaping from powerlevel10k to starship

--- setup/terminal.sh
+++ setup/terminal.sh
@@ -10,7 +10,7 @@ if test ! $(which omz); then
 fi
 
 # install Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # install a couple of zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
